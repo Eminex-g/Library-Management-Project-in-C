478e33a9fcc79d3dce685029303d3b6ff8326693
GitauSimone <64081189+Eminex-g@users.noreply.github.com>
2021-10-22 14:55:41 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Project Name: Library project
+Author: Simon Ndungu
+Date: 10/22/21
+*/
+
+int main()
+{
+    showInitialOptions();
+    return 0;
+}
+void showInitialOptions() {
+    printf("Log in\n");
+    printf("Add user\n");
+    printf("Add a book\n");
+    printf("Delete a book\n");
+    printf("Borrow a book\n");
+    printf("Return a book\n");
+    printf("Update my account\n");
+    printf("Log out\n");
+
+}
